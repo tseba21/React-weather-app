4eb8808d1044d2c5bc766556d84de237c74d1cfb
tseba21 <tsebaotmelese21@gmail.com>
2024-09-13 11:16:08 -0700

    added basic html

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.css";
+import Weather from "./weather.js";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <Weather />
   </React.StrictMode>
 );
 
---
+++ my-app/src/weather.js
@@ -0,0 +1,88 @@
+import React from "react";
+import Weather from "./weather";
+import "bootstrap/dist/css/bootstrap.css";
+
+export default function weather() {
+  return (
+    <div className="container weather-app">
+      <header>
+        <form id="search-form">
+          <input
+            type="search"
+            placeholder="Enter a city.."
+            required
+            class="search-input"
+            id="search-input"
+          />
+          <input type="submit" value="Search" class="search-button" />
+        </form>
+
+        <div className="row">
+          <div className="col">
+            <h1 className=" current-city">Addis Ababa</h1>
+
+            <div className="current-weather d-flex ">
+              <p>
+                Monday 00:58, thunder,rain
+                <br />
+                Humidity: 90%, Wind: 6.17km/h
+              </p>
+
+              <div className="col current-temperature d-flex justify-content-end">
+                🌩 17°C
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <div className="row current-temperature-icon">
+        <div className="col ">
+          mon
+          <br />
+          🌩
+          <p>17°C</p>
+        </div>
+        <div className="col ">
+          tus
+          <br />
+          🌤
+          <p>14°C</p>
+        </div>
+        <div className="col">
+          wed
+          <br />☀<p>27°C</p>
+        </div>
+        <div className="col">
+          thu
+          <br />❄<p> 4°C</p>
+        </div>
+        <div className="col">
+          fri
+          <br />
+          🌧
+          <p>19°C</p>
+        </div>
+      </div>
+      <footer>
+        <p>
+          This project was coded by
+          <a
+            href="https://www.github.com/tseba21"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Tsebaot Melese
+          </a>
+          and is
+          <a href="" target="_blank" rel="noreferrer">
+            on GitHub
+          </a>
+          and
+          <a href="" target="_blank" rel="noreferrer">
+            hosted on Netlify
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
+}
